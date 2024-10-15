8059456a8c263af814bfb0fcb7640e970a3e50eb
sanjana <sanjana@jsanjana555@gmail.com>
2024-10-14 22:54:56 -0400

    Deleted root-main.tf and root-output.tf

--- tf-own-module-vpc/root-output.tf
+++
@@ -1,9 +0,0 @@
-output "vpc" {
-  value = module.my-test-vpc.vpc_id
-}
-output "public_subnet_id" {
-  value = module.my-test-vpc.public_subnet_id
-}
-output "private_subnet_id" {
-  value = module.my-test-vpc.private_subnet_id
-}
